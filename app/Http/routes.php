9fd8ee786b6a4a16b44af60eadbf3ddaa1bad5df
Toni Sánchez <toni@grupowerk.com>
2015-11-07 12:45:28 +0100

    Create first View and fix some errors with gulp

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['uses' => 'WebsiteController@index', 'as' => 'website.index']);
--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,9 @@ var sourcemap = "resources/assets/";
 elixir(function(mix) {
     mix.sass(sourcemap + 'sass/main.scss', 'resources/assets/css/main.css');
     mix.sass(sourcemap + 'sass/ie8.scss', 'resources/assets/css/ie8.css');
-    mix.copy(sourcemap + 'css', 'public/css');
+
+    mix.copy(sourcemap + 'css', 'public/assets/css');
     mix.copy(sourcemap + 'images', 'public/images');
+    mix.copy(sourcemap + 'fonts', 'public/assets/fonts');
+    mix.copy(sourcemap + 'js', 'public/assets/js');
 });
